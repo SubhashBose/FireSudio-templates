8d9017090fe889e6266f899c1c8c02407cbbd034
Subhash Bose <online@subhashbose.com>
2025-04-06 04:46:34 +0200

    updt

--- all-in-one/dev.nix
+++ all-in-one/dev.nix
@@ -77,7 +77,7 @@
         # Open editors for the following files by default, if they exist:
         python-setup = ''
           mv -f .jupyter ../
-          python -m ipykernel install --name Python3.11 --display-name 'Python 3.11' --user;
+          #python -m ipykernel install --name Python3.11 --display-name 'Python 3.11' --user;
           python3 -m venv .venv --system-site-packages
           python3 -c 'import pandas, os; print(os.path.normpath(os.path.join(pandas.__file__,"../..")))' >> `ls -d .venv/lib/*/site-packages`/addpath.pth
           source .venv/bin/activate; 
