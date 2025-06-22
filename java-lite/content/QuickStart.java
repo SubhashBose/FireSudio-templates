1cfeafe52077ad4d1f8f23374461a6b63e0da190
Subhash Bose <online@subhashbose.com>
2025-06-22 22:58:45 +0000

    added java sample

---
+++ java-lite/content/QuickStart.java
@@ -0,0 +1,8 @@
+public class QuickStart {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java World!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
